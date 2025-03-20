6e73e8384b63491b4421f00e0a5a5129a238d14a
Bashir Okala <bashirokala@hotmail.com>
2025-03-20 06:48:15 +0100

    Project setup

---
+++ src/main/java/com/example/bookingstore/BookingstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookingstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingstoreApplication.class, args);
+    }
+
+}
